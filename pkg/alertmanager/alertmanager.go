dc3c457ceb0b45946a46b712b0b0a12c025c0f73
fayzal-g <fayzal.ghantiwala@grafana.com>
2024-07-02 14:42:23 +0100

    Use lock around templates

--- pkg/alertmanager/alertmanager.go
+++ pkg/alertmanager/alertmanager.go
@@ -123,6 +123,7 @@ type Alertmanager struct {
 	registry        *prometheus.Registry
 	receiversMtx    sync.Mutex
 	receivers       []*nfstatus.Receiver
+	templatesMtx    sync.RWMutex
 	templates       []string
 
 	// Pipeline created during last ApplyConfig call. Used for testing only.
@@ -352,7 +353,11 @@ func (am *Alertmanager) TestReceiversHandler(w http.ResponseWriter, r *http.Requ
 			http.StatusBadRequest)
 	}
 
-	response, err := alertingNotify.TestReceivers(r.Context(), c, am.templates, am.buildGrafanaReceiverIntegrations, am.cfg.ExternalURL.String())
+	am.templatesMtx.RLock()
+	tmpls := am.templates
+	am.templatesMtx.RUnlock()
+
+	response, err := alertingNotify.TestReceivers(r.Context(), c, tmpls, am.buildGrafanaReceiverIntegrations, am.cfg.ExternalURL.String())
 	if err != nil {
 		http.Error(w,
 			fmt.Sprintf("error testing receivers: %s", err.Error()),
@@ -383,6 +388,10 @@ func clusterWait(position func() int, timeout time.Duration) func() time.Duratio
 
 // ApplyConfig applies a new configuration to an Alertmanager.
 func (am *Alertmanager) ApplyConfig(conf *definition.PostableApiAlertingConfig, tmpls []string, rawCfg string, tmplExternalURL *url.URL) error {
+	am.templatesMtx.Lock()
+	am.templates = tmpls
+	am.templatesMtx.Unlock()
+
 	tmplsReader := make([]io.Reader, len(tmpls))
 	for _, tmplString := range tmpls {
 		tmplsReader = append(tmplsReader, strings.NewReader(tmplString))
@@ -393,8 +402,6 @@ func (am *Alertmanager) ApplyConfig(conf *definition.PostableApiAlertingConfig, 
 		return err
 	}
 	tmpl.ExternalURL = tmplExternalURL
-
-	am.templates = tmpls
 
 	cfg := definition.GrafanaToUpstreamConfig(conf)
 	am.api.Update(&cfg, func(_ model.LabelSet) {})
